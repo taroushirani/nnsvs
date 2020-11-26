98b54e859feff87190ca0d7cab08fde38eb2eef8
Tarou Shirani <taroushirani@gmail.com>
2020-11-26 15:56:27 +0900

    Minor fix

--- nnsvs/dar.py
+++ nnsvs/dar.py
@@ -66,7 +66,7 @@ class MDNDAR(nn.Module):
         
         for idx in range(T):
             if idx == 0:
-                inputs = torch.cat((x[:,idx,:], torch.zeros(B, self.out_dim), device=x.device), dim=1)
+                inputs = torch.cat((x[:,idx,:], torch.zeros(B, self.out_dim, device=x.device)), dim=1)
             else:
                 inputs = torch.cat((x[:,idx,:], self.dropout(mu[:, idx-1, :])), dim=1)
                 _lp, _ls, _m, hidden = self.mdndarcell(inputs, hidden)
