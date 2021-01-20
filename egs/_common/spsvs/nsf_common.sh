0ce5caab2b19ae013a53c274a46546a4a52c9b84
Tarou Shirani <taroushirani@gmail.com>
2021-01-21 00:58:50 +0900

    Add nsf common setting

---
+++ egs/_common/spsvs/nsf_common.sh
@@ -0,0 +1,8 @@
+# NOTE: the script is supposed to be used called from nnsvs recipes.
+# Please don't try to run the shell script directry.
+
+nsf_input_dirs=$expdir/nsf/input_dirs
+nsf_output_dirs=$expdir/nsf/output_dirs
+nsf_test_input_dirs=$expdir/nsf/test_input_dirs
+nsf_test_output_dirs=$expdir/nsf/test_output_dirs
+nsf_save_model_dir=$expdir/nsf/train_outputs
